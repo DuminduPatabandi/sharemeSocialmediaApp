f4a35e798307480761de6803cd73a5e30fb0c1c2
DuminduPatabandi <dppatabandi@gmail.com>
2023-01-30 20:51:51 +0530

    phase3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+createRoot(
+    document.getElementById('root')).render(
+        <Router>
+        <App />
+        </Router>
+    );
+
